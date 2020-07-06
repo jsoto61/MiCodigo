f19af82da2a8715c97431390eb3b194827e38a76
jsoto61 <48635750+jsoto61@users.noreply.github.com>
2020-07-06 18:32:43 +0100

    Se generaliza el número de seguimiento con XXXXXX

--- Insertar-codigo-de-analytics.php
+++ Insertar-codigo-de-analytics.php
@@ -6,9 +6,9 @@ add_action('wp_head','du_google_analytics');
 	function du_google_analytics() {
 
 		echo '<!-- Global site tag (gtag.js) - Google Analytics -->';
-		echo '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-45993564-35"></script>';
+		echo '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX"></script>';
 		echo '<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}';
   		echo "gtag('js', new Date());";
-		echo "gtag('config', 'UA-45993564-35');</script>";
+		echo "gtag('config', 'UA-XXXXXXXXX');</script>";
 
 	}
