66865cf0519b617e80ae79cd45527197b1ef1d86
jsoto61 <48635750+jsoto61@users.noreply.github.com>
2020-09-20 15:02:52 +0100

    ver_0.1

--- Añadir imagen de cabecera en página de entradas.php
+++ Añadir imagen de cabecera en página de entradas.php
@@ -2,25 +2,32 @@
 /**
  * Duando.
  * @link    https://duando.net
-
-CSS NECESARIO
-.du-imagen-hero-blog {
-	background-image: url("/wp-content/...url de la imagen");
-	background-color:transparent;
-	max-width: 2200px;
-	height: 440px;
-	background-size: cover;
-	background-position:bottom ;
-	margin: auto;	
-}
+ * este script debe colocarse en un archivo creado en 
+ * /wp-content/themes/genesis-sample/home.php
+ * CSS NECESARIO
+ * .du-imagen-hero-blog {
+ * 	background-image: url("/wp-content/...url de la imagen");
+ * 	background-color:transparent;
+ * 	max-width: 2200px;
+ * 	height: 440px;
+ * 	background-size: cover;
+ * 	background-position:bottom ;
+ * 	margin: auto;	
+ * }
 
  */
 
 /*Inserta la imagen de cabecera para el blog*/
-if (is_home) {
+
+function du_imagen_hero() {
+    echo '<div class="du-imagen-hero-blog"></div>';
+    }
+
 add_action('genesis_after_header','du_imagen_hero');
-    function du_imagen_hero() {
-		echo '<div class="du-imagen-hero-blog"></div>';
-}
-}
+
+
+// Initialize Genesis.
+
+genesis();
+/*hasta aquí*/
 ?>
