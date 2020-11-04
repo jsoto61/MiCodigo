b6a7dafe156c850cb6f04f3a9cba67ff7ba0bba3
jsoto61 <48635750+jsoto61@users.noreply.github.com>
2020-11-04 22:23:54 +0000

    nuevo script

---
+++ insertar-leermas-tras-excerpt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+     * Añadir puntos suspensivos al extracto and mostrar un botón "Leer Más" .
+     * 
+     * @param type $text
+     * @return string
+     */
+	function custom_excerpt($text) {
+		//$text= substr_replace($text,"...",strpos($text, "</p>"),0);
+		$excerpt = $text . '<a href="' . get_permalink() . '">
+		<button class="du-read-more-btn" type="button" value="read_more">Leer más...</button></a>';
+		return $excerpt;
+		}
+	
+        add_filter('the_excerpt', 'custom_excerpt');
+        
+        ?>
