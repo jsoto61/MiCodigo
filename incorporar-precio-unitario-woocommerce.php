715dbefa5c5c97bd596c9e43b8feda1605e65a11
jsoto61 <48635750+jsoto61@users.noreply.github.com>
2020-12-05 16:16:33 +0000

    primera versión

---
+++ incorporar-precio-unitario-woocommerce.php
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * 	
+ *  RUTINA PARA AÑADIR AL PRECIO DE UN PRODUCTO DE WOOCOMMERCE UN TEXTO QUE 
+ *  INDIQUE  EL COSTE POR VOLUMEN, PESO O UNITARIO.
+ *  Para que funcione se debe crear un campo personalizado en el producto de 
+ *  woocommerce denominado precio-unidad-peso-volumen (si se modifica este 
+ *  nombre recordar cambiarlo también en la función)
+ * 
+ *******************************************************************************/
+
+	function du-filtro-woocommerce-precio( $price, $instance ) { 
+		$du_precio_unidad_peso_volumen = get_post_meta( get_the_ID(), 'precio-unidad-peso-volumen', true );
+	
+		if ($du_precio_unidad_peso_volumen) 
+			$price =  $price . '<br> <span class="du-precio-unitario"> (' . $du_precio_unidad_peso_volumen .')<span>' ;
+		return $price; 
+		}
+	
+	add_filter( 'woocommerce_get_price_html', 'du-filtro-woocommerce-precio', 10, 2 );
